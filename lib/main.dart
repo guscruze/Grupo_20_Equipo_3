00ee8bfd4fa48b2c5646fa1ba52e9c5df88a1072
guscruze <91161318+guscruze@users.noreply.github.com>
2021-12-14 07:39:19 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:aprendiendomystore/view/home_view.dart';
+import 'package:aprendiendomystore/persistence/stores_dao.dart';
 import 'package:flutter/material.dart';
-import 'ui/listview_product.dart';
 
-void main() => runApp(new MaterialApp(
-  home: ListViewProduct(),
-  title: 'Product Db',
-));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  StoresDAO.addStoresFromServer().then((value){
+    runApp(HomeView());
+  });
+}
 
